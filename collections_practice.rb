2b2c694b678080c59f1c06fd7357e9b51593bb2e
David zhang <d.ydzdavid@yahoo.com>
2019-04-02 20:13:54 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,74 @@
 
+def sort_array_asc(interger)
+  interger.sort 
+end
+
+
+def sort_array_desc(interger)
+  interger.sort.reverse
+end
+
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+
+
+
+def swap_elements(array)
+  array = array[0], array[2], array[1]
+end
+
+
+
+
+def reverse_array(array)
+  array.reverse 
+end
+
+
+
+
+def kesha_maker(array)
+  array.each {|sign| sign[2] = "$"}
+end
+
+
+
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+end
+
+
+
+
+def sum_array(array)
+  array.inject {|result, element| result + element}
+end
+
+
+
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| index==1?"#{element}":"#{element}s"}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
